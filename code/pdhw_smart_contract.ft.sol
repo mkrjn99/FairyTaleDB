f0f50a437f5946c063548c416f947879ad8acb3e
Mayank Rajani <mkrjn99@gmail.com>
2025-02-17 08:17:23 +0530

    more progress in the PDHW contract

--- code/pdhw_smart_contract.ft.sol
+++ code/pdhw_smart_contract.ft.sol
@@ -1,4 +1,4 @@
-// Auto-generated code: https://chatgpt.com/c/67b29926-32b8-8004-8bd4-eba71198078f
+// Semi-auto-generated code: https://chatgpt.com/c/67b29926-32b8-8004-8bd4-eba71198078f
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -12,14 +12,14 @@ contract PDHWToken {
 
     address public owner;
     address public pending_payments_wallet_id;
-    mapping(address => uint256) public balances;
+    mapping(address => int256) public balances;
     mapping(address => bool) public negative_balance_allowed;
     mapping(address => mapping(address => uint256)) public allowances;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event LtcrInrUpdated(uint256 newValue);
-    event BalanceUpdated(address indexed wallet, uint256 newBalance);
+    event BalanceUpdated(address indexed wallet, int256 newBalance);
     event NegativeBalanceAllowed(address indexed wallet);
 
     modifier onlyOwner() {
@@ -37,13 +37,19 @@ contract PDHWToken {
         pending_payments_wallet_id = _pending_payments_wallet_id;
     }
 
+    function truncateUint256ToInt128(uint256 input) public pure returns (int128) {
+        input &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+        require(input >= 0, "truncated int128 value should be positive");
+        return int128(int256(input));
+    }
+
     function setLtcrInr(uint256 val) external onlyOwner {
         ltcr_inr = val;
-        balances[pending_payments_wallet_id] = pending_payments / ltcr_inr;
+        balances[pending_payments_wallet_id] = truncateUint256ToInt128(pending_payments / ltcr_inr);
         emit LtcrInrUpdated(val);
     }
 
-    function setBalance(address wallet_id, uint256 balance) external onlyOwner {
+    function setBalance(address wallet_id, int256 balance) external onlyOwner {
         require(wallet_id != pending_payments_wallet_id, "Cannot set balance for pending payments wallet");
         balances[wallet_id] = balance;
         emit BalanceUpdated(wallet_id, balance);
@@ -51,11 +57,11 @@ contract PDHWToken {
 
     function transfer(address from, address to, uint256 amount) external onlyWallet(from) {
         require(
-            negative_balance_allowed[from] || (amount + transaction_fee) <= balances[from],
+            negative_balance_allowed[from] || (amount + transaction_fee) <= uint256(balances[from]),
             "Insufficient balance or negative balance not allowed"
         );
-        balances[from] -= (amount + transaction_fee);
-        balances[to] += amount;
+        balances[from] -= int256(amount + transaction_fee);
+        balances[to] += int256(amount);
         emit Transfer(from, to, amount);
     }
 
@@ -71,11 +77,12 @@ contract PDHWToken {
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
-        require(amount <= balances[sender], "Insufficient balance");
+        require(balances[sender] > 0, "transferFrom should be called by positive balance holders only");
+        require(int256(amount) <= balances[sender], "Insufficient balance");
         require(amount <= allowances[sender][msg.sender], "Allowance exceeded");
 
-        balances[sender] -= amount;
-        balances[recipient] += amount;
+        balances[sender] -= int256(amount);
+        balances[recipient] += int256(amount);
         allowances[sender][msg.sender] -= amount;
 
         emit Transfer(sender, recipient, amount);
